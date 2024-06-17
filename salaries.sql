528847d15b9a58fe9f56b7c785eeeb3936efe58c
THEJASHWANI <141561328+THEJASHWANI@users.noreply.github.com>
2024-06-17 09:36:57 +0530

    Add files via upload

---
+++ salaries.sql
@@ -0,0 +1,13 @@
+CREATE TABLE salaries (
+    employee_id INT,
+    salary DECIMAL(10, 2),
+    from_date DATE,
+    to_date DATE,
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);
+SELECT e.first_name, e.last_name, d.department_name, s.salary
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN salaries s ON e.employee_id = s.employee_id
+ORDER BY s.salary DESC
+LIMIT 5;
